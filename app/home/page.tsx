8bb3d7708556e29313e6874df822f26003f53593
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-08-13 11:46:22 +0930

    update import path

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,5 @@
 import SearchForm from '@/app/ui/home/search-form';
-import Hero from '@/app/ui/hero-section';
+import Hero from '@/app/ui/home/hero-section';
 
 export default function Home() {
   return (
