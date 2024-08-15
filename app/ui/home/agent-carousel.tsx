4b080bb2bbe445d1ce3507bf72959750f51e487a
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-08-15 14:45:42 +0930

    feat:create a title for agent carousel

--- app/ui/home/agent-carousel.tsx
+++ app/ui/home/agent-carousel.tsx
@@ -83,6 +83,9 @@ export default function AgentCarousel() {
 
   return (
     <div className="relative w-full my-16 max-w-3xl mx-auto p-6 bg-blue-50 rounded-lg shadow-lg">
+      <h2 className="text-3xl font-bold text-center text-blue-600 mb-12">
+        Client Feedbacks
+      </h2>
       <div className="flex justify-between items-center">
         <button onClick={handlePrev}>
           <FaChevronLeft className="text-blue-500 text-2xl" />
