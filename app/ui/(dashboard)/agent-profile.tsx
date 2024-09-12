72acfee0c9a2b32bb46231b6764bbc48ef2e6927
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-09-12 12:42:55 +0930

    feat:create a protected profile which can be accessed only by providing token

---
+++ app/ui/(dashboard)/agent-profile.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter, usePathname } from 'next/navigation';
+
+export default function AgentProfile() {
+  const [loadingProfile, setLoadingProfile] = useState(false);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { data: session } = useSession();
+
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  useEffect(() => {
+    if (session?.user?.accessToken) {
+      // Fetch user profile if access token is available
+      getUserProfile(session.user.accessToken);
+    } else {
+      // Redirect to `/signin` if no access token or no session
+      router.push('/signin');
+    }
+  }, []);
+
+  const getUserProfile = (token: string) => {
+    setLoadingProfile(true);
+    fetch(`${baseUrl}/auth/user_profile`, {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+      .then((response) => {
+        setLoadingProfile(false);
+      })
+      .catch((error) => {
+        // handle error here
+        console.error(error);
+      });
+  };
+  return (
+    <>
+      {' '}
+      {loadingProfile ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <p>User Profile</p>
+          <p>Name: {session?.user?.name}</p>
+          <p>Email: {session?.user?.email}</p>
+        </div>
+      )}
+    </>
+  );
+}
