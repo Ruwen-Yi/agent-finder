5a6e20775e97e6a5cc32f88c292f5573cc75f380
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-10-16 16:09:44 +1030

    feat:create a function to fetch the agent data by agent id

---
+++ app/lib/data.ts
@@ -0,0 +1,12 @@
+import prisma from '@/app/lib/prisma';
+
+// fetch an agent data with its id
+export async function fetchAgent(agentId: string) {
+	const result = await prisma.agent.findUnique({
+		where: {
+			id: agentId,
+		}
+	});
+
+	return result;
+}
