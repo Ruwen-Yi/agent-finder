76efd13c889d76b3315a4e9f1cd8511da68646ff
Ruwen-Yi <ruwen.yi@hotmail.com>
2024-10-16 16:26:33 +1030

    feat:find more information when searching an agent in database

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,7 +5,12 @@ export async function fetchAgent(agentId: string) {
 	const result = await prisma.agent.findUnique({
 		where: {
 			id: agentId,
-		}
+		},
+		include: {
+			address: true,
+			services: true,
+			specializations: true,
+		},
 	});
 
 	return result;
